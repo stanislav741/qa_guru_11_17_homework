0fc88bcd506ab2b6653893a21d8b9ec4c354e963
stanislav741 <stasdmitruk1@gmail.com>
2022-05-09 22:13:27 +0200

    Added new methods for getting list of resources and a single user info

--- src/test/java/tests/ReqresTest.java
+++ src/test/java/tests/ReqresTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
+import static org.assertj.guava.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReqresTest {
 
@@ -13,10 +15,10 @@ public class ReqresTest {
 
 //        Response: {"id":4,"token":"QpwL5tke4Pnpja7X4"}
 
-        String registerData = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\"}";
+        String bodyUserRegister = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\"}";
 
         given().
-                body(registerData).
+                body(bodyUserRegister).
                 contentType(JSON).
         when().
                 post("https://reqres.in/api/register").
@@ -28,10 +30,10 @@ public class ReqresTest {
     @Test
     void apiUserLogin() {
 
-        String loginData = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\"}";
+        String bodyUserLogin = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\"}";
 
         given().
-                body(loginData).
+                body(bodyUserLogin).
                 contentType(JSON).
         when().
                 post("https://reqres.in/api/login").
@@ -39,4 +41,50 @@ public class ReqresTest {
                 statusCode(200).
                 body("token", is("QpwL5tke4Pnpja7X4"));
     }
+
+    @Test
+    void apiResourceList() {
+
+    String responseResourceListExpected = "{\"page\":1,\"per_page\":6,\"total\":12,\"total_pages\":2,\"data\":" +
+            "[{\"id\":1,\"name\":\"cerulean\",\"year\":2000,\"color\":\"#98B2D1\",\"pantone_value\":\"15-4020\"}," +
+            "{\"id\":2,\"name\":\"fuchsia rose\",\"year\":2001,\"color\":\"#C74375\",\"pantone_value\":\"17-2031\"}," +
+            "{\"id\":3,\"name\":\"true red\",\"year\":2002,\"color\":\"#BF1932\",\"pantone_value\":\"19-1664\"}" +
+            ",{\"id\":4,\"name\":\"aqua sky\",\"year\":2003,\"color\":\"#7BC4C4\",\"pantone_value\":\"14-4811\"}," +
+            "{\"id\":5,\"name\":\"tigerlily\",\"year\":2004,\"color\":\"#E2583E\",\"pantone_value\":\"17-1456\"}," +
+            "{\"id\":6,\"name\":\"blue turquoise\",\"year\":2005,\"color\":\"#53B0AE\",\"pantone_value\":" +
+            "\"15-5217\"}],\"support\":{\"url\":\"https://reqres.in/#support-heading\"," +
+            "\"text\":\"To keep ReqRes free, contributions towards server costs are appreciated!\"}}";
+
+    String responseResourceList =
+        given().
+                contentType(JSON).
+        when().
+                get("https://reqres.in/api/unknown").
+        then().
+                statusCode(200).
+                extract().response().asString();
+
+        assertEquals(responseResourceListExpected, responseResourceList);
+    }
+
+    @Test
+    void apiUserSingle() {
+
+    String responseUserSingleExpected = "{\"data\":{\"id\":2,\"email\":\"janet.weaver@reqres.in\",\"first_name\":" +
+                "\"Janet\",\"last_name\":\"Weaver\",\"avatar\":\"https://reqres.in/img/faces/2-image.jpg\"}," +
+                "\"support\":{\"url\":\"https://reqres.in/#support-heading\"," +
+                "\"text\":\"To keep ReqRes free, contributions towards server costs are appreciated!\"}}";
+
+    String responseUserSingle =
+        given().
+                contentType(JSON).
+        when().
+                get("https://reqres.in/api/users/2").
+        then().
+                statusCode(200).
+                extract().response().asString();
+
+        assertEquals(responseUserSingleExpected, responseUserSingle);
+    }
+
 }
